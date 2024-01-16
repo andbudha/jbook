808cdad0fc6cc6198665c4889ae5808906c64ec5
andbudha <andreibartov@gmail.com>
2024-01-16 07:53:45 +0100

    starting esbuild service implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React, { ChangeEvent, useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 function App() {
+  const ref = useRef<any>();
   const [txtAreaValue, setTxtAreaValue] = useState('');
   const [resultedCode, setResultedCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '../public/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const valueCatchingHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setTxtAreaValue(event.currentTarget.value);
   };
 
   const transpileHandler = () => {
-    setResultedCode(txtAreaValue);
+    if (!ref.current) {
+      return;
+    }
+    console.log(ref.current);
   };
   return (
     <div className="App">
